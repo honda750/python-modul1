d7e146588b556db75c348d5b989fb80e447010ea
honda750 <ksdraege@gmail.com>
2017-11-01 20:20:16 +0100

    first commit
    Første fil på Github fra PyCharm Edu 4.02 på kurs 01.04.17

---
+++ Radius.py
@@ -0,0 +1,10 @@
+#Program 2
+#Dette er en Variabel
+radius  = 5
+
+#This is a constant
+myConstPi = 3.14
+
+print("The Radius is in the circle is", radius)
+
+print("The circumference of the circle in integer is:", int(2*myConstPi*radius))
