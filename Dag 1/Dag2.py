38f978fe2f19180b538d29ba1d5ba00a00a3f8d6
kurtsteinardraege <ksdraege@gmail.com>
2017-11-08 18:15:14 +0100

    Dag 2 prog 1

---
+++ Dag 1/Dag2.py
@@ -0,0 +1,15 @@
+print('Kurt Steinar')
+alder1 = 64
+print('Jeg er',alder1,'år gammel' )
+print (2077 - 1953)
+alder = int(input('Hvor gammel er du? '))
+print('Du er',alder,'år gammel')
+årstall = int(input('Skriv inn et årstall i fremtiden: '))
+født= int(input('Fødselsår:' ))
+print('Alderen min i',årstall,'er',årstall-født)
+
+# 1) Enterred a string - breaks
+# 2) Enter a desimal - breaks
+# 3) Empty string
+# 4) a space
+# OBS input er defalt string
