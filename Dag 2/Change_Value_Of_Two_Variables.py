69791d8fdd1a0c3a2c1d6c734385d2e6b5975190
kurtsteinardraege <ksdraege@gmail.com>
2017-12-06 11:21:45 +0100

    New way to change the variables.

--- Dag 2/Change_Value_Of_Two_Variables.py
+++ Dag 2/Change_Value_Of_Two_Variables.py
@@ -8,7 +8,12 @@ print('Variable one = ',VariableOne)
 print('Variable two = ',VariableTwo)
 
 # Change the values of the variables
-VariableOne, VariableTwo = VariableTwo, VariableOne
+
+# VariableOne, VariableTwo = VariableTwo, VariableOne   # First solution OK!
+
+tempVariable = VariableOne
+VariableOne = VariableTwo
+VariableTwo = tempVariable
 
 print('Variable one are now = ',VariableOne)
 print('Variable two are now = ',VariableTwo)
