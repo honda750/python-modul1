925c36e221e6ab433796c138ae6617a7b6188a96
kurtsteinardraege <ksdraege@gmail.com>
2017-12-06 13:30:50 +0100

    Utvidet program

--- Privat/Dato og tid/Tid.py
+++ Privat/Dato og tid/Tid.py
@@ -1,12 +1,40 @@
 """
     Tid og Dato
 """
-from datetime import datetime as tid
+from datetime import datetime as time
 
-iDag = tid.now()
+def dato():                                         # Dato, måned, år
+    date = time.strftime(time.now(), '%d.%m.%Y')
+    return date
 
-dato = tid.strftime(iDag, '%d.%m.%Y')
-klokkeslett = tid.strftime(iDag, '%H:%M:%S')
+def tidS():                                          # Time, minutt, sekund
+    clock = time.strftime(time.now(), '%H:%M:%S')
+    return clock
 
-print(dato)
-print(klokkeslett)
+def tid():                                          # Time, minutt
+    clock = time.strftime(time.now(), '%H:%M')
+    return clock
+
+def dag():                                          # Dag
+    day = time.strftime(time.now(), '%A')
+    if (day == 'Monday'):
+        day = 'Mandag'
+    elif (day == 'Tuesday'):
+        day = 'Tirsdag'
+    elif (day == 'Wednesday'):
+        day = 'Onsdag'
+    elif (day == 'Thursday'):
+        day = 'Torsdag'
+    elif (day == 'Friday'):
+        day = 'Fredag'
+    elif (day == 'Saturday'):
+        day = 'Lørdag'
+    elif (day == 'Sunday'):
+        day = 'Søndag'
+    return day
+
+
+print(dato())
+print(tidS())
+print(dag())
+print(dag(), dato(), tid())
